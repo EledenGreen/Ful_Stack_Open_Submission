d7e77dfc9b0ef682cced5695a2e89aa6f6741a9b
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-01-21 09:35:22 +0530

    uibuild

--- Exercises/Part-3/phonebook/index.js
+++ Exercises/Part-3/phonebook/index.js
@@ -19,6 +19,7 @@ const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint'})
 }
 
+app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'));
