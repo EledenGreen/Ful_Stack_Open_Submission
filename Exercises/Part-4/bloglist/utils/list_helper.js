1e353afcb33eeb0c0cbdb4bf152f9441a5787d71
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-04-13 21:19:51 +0530

    4.5 implemented

--- Exercises/Part-4/bloglist/utils/list_helper.js
+++ Exercises/Part-4/bloglist/utils/list_helper.js
@@ -10,7 +10,29 @@ const totalLikes = (blogs) => {
     return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    let i = 0
+    let max = 0
+    let max_i = 0
+
+    if(blogs.length === 0)
+        return 0
+
+    while(i < blogs.length)
+    {
+        if(blogs[i].likes > max)
+            {
+                max = blogs[i].likes
+                max_i = i
+            }
+        i++
+    }
+
+    return blogs[max_i]
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
