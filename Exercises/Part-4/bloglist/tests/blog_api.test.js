8ced9e6e77a8d0505a55f0f73e2c016dc775710b
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-04-17 11:43:13 +0530

    4.10 implemented

--- Exercises/Part-4/bloglist/tests/blog_api.test.js
+++ Exercises/Part-4/bloglist/tests/blog_api.test.js
@@ -20,18 +20,34 @@ beforeEach(async () => {
     console.log('done')
 })
 
-test('all blogs are returned', async () => {
+test('all blogs are returned using GET', async () => {
     const contents = await api.get('/api/blogs')
 
     assert.strictEqual(contents.body.length, helper.initialBlogs.length)
 })
 
-test.only('unique identifier is "id" not "_id" ', async () => {
+test('unique identifier is "id" not "_id" ', async () => {
     const contents = await api.get('/api/blogs')
 
     for(let blog of contents.body)
         if ('id' in blog && !('_id' in blog)) console.log('id')
 
+})
+
+test.only('a new blog can be added using POST', async () => {
+    const content = helper.listWithOneBlog
+    console.log(content)
+    await api
+    .post('/api/blogs')
+    .send(content)
+    .expect(201)
+    .expect('Content-type', /application\/json/)
+
+    const blogsAtEnd = await api.get('/api/blogs')
+    const response = blogsAtEnd.body.map(r => r.title)
+
+    assert.strictEqual(blogsAtEnd.body.length, helper.initialBlogs.length + 1)
+    assert(response.includes('Go To Statement Considered Harmful'))
 })
 
 after(async () => {
