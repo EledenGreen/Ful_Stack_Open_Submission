53c8896037eab6b13475b8c372cae0dab61dce5a
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-04-26 16:17:52 +0530

    4.16 implemented

--- Exercises/Part-4/bloglist/tests/test_helper.js
+++ Exercises/Part-4/bloglist/tests/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const blogToPatch = {
   likes: 10
@@ -69,10 +70,16 @@ const blogsInDb = async () => {
     return blogs.map(blog => blog.toJSON())
 }
 
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(u => u.toJSON())
+}
+
 module.exports = {
     initialBlogs,
     blogsInDb,
     listWithOneBlog,
     listWithNoTitle,
-    blogToPatch
+    blogToPatch,
+    usersInDb
 }
