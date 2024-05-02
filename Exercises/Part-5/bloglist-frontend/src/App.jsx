1b5922fb127910acc75470ee1b3514618fda24a0
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-05-02 16:49:51 +0530

    5.1 implemented
    renders automatically after logged-in

--- Exercises/Part-5/bloglist-frontend/src/App.jsx
+++ Exercises/Part-5/bloglist-frontend/src/App.jsx
@@ -1,19 +1,96 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import React, { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import blogService from './services/blogs';
+import loginService from './services/login';
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser');
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
+
+      blogService.getAll().then(initialBlogs => {
+        setBlogs(initialBlogs);
+      });
+    }
+  }, []);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({
+        username, password,
+      });
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      );
+
+      blogService.setToken(user.token);
+      setUser(user);
+      setUsername('');
+      setPassword('');
+
+      blogService.getAll().then(initialBlogs => {
+        setBlogs(initialBlogs);
+      });
+    } 
+    catch (exception) {
+      setErrorMessage('Wrong Credentials');
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        {loginForm()}
+
+      </div>
+    )
+  }
 
   return (
     <div>
-      <h2>blogs</h2>
+      <h2>Blogs</h2>
+      <p> {user.name} logged-in </p>
+
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
@@ -21,4 +98,4 @@ const App = () => {
   )
 }
 
-export default App
+export default App;
