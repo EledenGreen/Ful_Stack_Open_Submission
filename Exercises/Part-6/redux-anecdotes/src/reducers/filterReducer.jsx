696b63d1585febd7ce14c0217768c1919c1d1aa2
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-05-13 19:40:59 +0530

    6.10 implemented
    App working

--- Exercises/Part-6/redux-anecdotes/src/reducers/filterReducer.jsx
+++ Exercises/Part-6/redux-anecdotes/src/reducers/filterReducer.jsx
@@ -1,4 +1,19 @@
-const filterReducer = (state = '', action) => {
+import { createSlice } from "@reduxjs/toolkit";
+
+const filterSlice = createSlice({
+    name: 'filter',
+    initialState: '',
+    reducers: {
+        filterChange(state, action) {
+            return action.payload
+        }
+    }
+})
+
+export default filterSlice.reducer
+export const { filterChange } = filterSlice.actions
+
+/*const filterReducer = (state = '', action) => {
     switch (action.type) {
         case 'SET_FILTER':
             return action.payload
@@ -15,3 +30,4 @@ const filterChange = filter => {
 }
 
 export { filterChange, filterReducer }
+*/
