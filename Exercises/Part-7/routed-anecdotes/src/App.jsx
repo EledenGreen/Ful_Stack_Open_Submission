aa551b7aa697b136fbba3f9475683f6414085bb4
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-05-16 15:37:42 +0530

    7.4 implemented

--- Exercises/Part-7/routed-anecdotes/src/App.jsx
+++ Exercises/Part-7/routed-anecdotes/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import useField from './hooks'
+
 import {
   BrowserRouter as Router,
   Routes,
@@ -77,17 +79,16 @@ const Footer = () => (
 const CreateNew = (props) => {
   const navigate = useNavigate()
 
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
-
+  const content = useField('text', 'content')
+  const author = useField('text', 'author')
+  const info = useField('text', 'info')
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
     navigate('/')
@@ -99,15 +100,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
