cb05edc16a99644701458313eb93730321bdc7a9
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-10-18 19:09:15 +0530

    8.4 implemented

--- Exercises/Part-8/GraphQL-backend/index.js
+++ Exercises/Part-8/GraphQL-backend/index.js
@@ -117,7 +117,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -126,8 +126,11 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => {
-      return books
+    allBooks: (root, args) => {
+      if (!args.author) {
+        return books
+      }
+      return books.filter((book) => book.author === args.author)
     },
     allAuthors: () => {
       return authors
