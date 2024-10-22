aa8bba0b9451de43907003a88c38cbd2b4c04f8b
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-10-22 10:25:03 +0530

    logout added

--- Exercises/Part-8/GraphQL-frontend/src/App.jsx
+++ Exercises/Part-8/GraphQL-frontend/src/App.jsx
@@ -3,10 +3,19 @@ import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 import LoginForm from './components/LoginForm'
+import { useApolloClient } from '@apollo/client'
 
 const App = () => {
   const [page, setPage] = useState('authors')
   const [token, setToken] = useState(null)
+
+  const client = useApolloClient()
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
 
   return (
     <div>
@@ -18,7 +27,7 @@ const App = () => {
         ) : (
           <>
             <button onClick={() => setPage('add')}>add book</button>
-            <button>logout</button>
+            <button onClick={logout}>logout</button>
           </>
         )}
       </div>
