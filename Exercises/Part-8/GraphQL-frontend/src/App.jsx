8a0be1009fec7bf2dd669f1e856c7ae392507c3e
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-10-22 10:03:38 +0530

    LoginForm implemented

--- Exercises/Part-8/GraphQL-frontend/src/App.jsx
+++ Exercises/Part-8/GraphQL-frontend/src/App.jsx
@@ -1,26 +1,41 @@
-import { useState } from "react";
-import Authors from "./components/Authors";
-import Books from "./components/Books";
-import NewBook from "./components/NewBook";
+import { useState } from 'react'
+import Authors from './components/Authors'
+import Books from './components/Books'
+import NewBook from './components/NewBook'
+import LoginForm from './components/LoginForm'
 
 const App = () => {
-  const [page, setPage] = useState("authors");
+  const [page, setPage] = useState('authors')
+  const [token, setToken] = useState(null)
 
   return (
     <div>
       <div>
-        <button onClick={() => setPage("authors")}>authors</button>
-        <button onClick={() => setPage("books")}>books</button>
-        <button onClick={() => setPage("add")}>add book</button>
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        {token === null ? (
+          <button onClick={() => setPage('login')}>login</button>
+        ) : (
+          <>
+            <button onClick={() => setPage('add')}>add book</button>
+            <button>logout</button>
+          </>
+        )}
       </div>
 
-      <Authors show={page === "authors"} />
+      <Authors show={page === 'authors'} />
 
-      <Books show={page === "books"} />
+      <Books show={page === 'books'} />
 
-      <NewBook show={page === "add"} />
+      <NewBook show={page === 'add'} />
+
+      <LoginForm
+        setToken={setToken}
+        show={page === 'login'}
+        setPage={setPage}
+      />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
