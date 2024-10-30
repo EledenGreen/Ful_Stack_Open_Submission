ced35915d98c1b7c21d787e52a54c0520e890965
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-10-30 16:22:30 +0530

    9.11 implemented

--- Exercises/Part-9/patientor/backend/src/index.ts
+++ Exercises/Part-9/patientor/backend/src/index.ts
@@ -1,11 +1,18 @@
 import express from "express";
 import patientRouter from "./routes/patients";
 import diagnosesRouter from "./routes/diagnoses";
+import cors from "cors";
 
 const app = express();
+// eslint-disable-next-line
+app.use(cors());
 app.use(express.json());
 
 const PORT = 3001;
+
+app.use("/api/ping", (_req, res) => {
+  res.send("pong");
+});
 
 app.use("/api/patients", patientRouter);
 app.use("/api/diagnoses", diagnosesRouter);
