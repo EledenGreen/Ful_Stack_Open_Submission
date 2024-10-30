ed1074a843c02ad9f151120339ad4f32263caec0
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-10-31 03:48:05 +0530

    9.15 implemented

--- Exercises/Part-9/courseinfo/src/App.tsx
+++ Exercises/Part-9/courseinfo/src/App.tsx
@@ -1,34 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const App = () => {
+  const courseName = 'Half Stack application development'
+  const courseParts = [
+    {
+      name: 'Fundamentals',
+      exerciseCount: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exerciseCount: 7,
+    },
+    {
+      name: 'Deeper type usage',
+      exerciseCount: 14,
+    },
+  ]
 
-function App() {
-  const [count, setCount] = useState(0)
+  interface HeaderProps {
+    courseName: string
+  }
+
+  const Header = (props: HeaderProps) => {
+    return <h2>{props.courseName}</h2>
+  }
+
+  interface courseParts {
+    name: string
+    exerciseCount: number
+  }
+
+  interface ContentProps {
+    courseParts: courseParts[]
+  }
+
+  const Content = (props: ContentProps) => {
+    return (
+      <>
+        {props.courseParts.map((item, index) => (
+          <div key={index}>
+            {item.name} {item.exerciseCount}
+          </div>
+        ))}
+      </>
+    )
+  }
+
+  const Total = (props: ContentProps) => {
+    const total = props.courseParts.reduce(
+      (sum, part) => sum + part.exerciseCount,
+      0
+    )
+
+    return <div>Number of exercises: {total}</div>
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Header courseName={courseName} />
+      <Content courseParts={courseParts} />
+      <Total courseParts={courseParts} />
+    </div>
   )
 }
 
