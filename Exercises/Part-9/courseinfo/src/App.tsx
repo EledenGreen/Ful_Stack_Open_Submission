891c3fd43cfa315745ab0ab687088b0273796c2f
EledenGreen <114846457+EledenGreen@users.noreply.github.com>
2024-10-31 08:27:26 +0530

    9.16 implemented

--- Exercises/Part-9/courseinfo/src/App.tsx
+++ Exercises/Part-9/courseinfo/src/App.tsx
@@ -1,17 +1,81 @@
+import './App.css'
+
 const App = () => {
   const courseName = 'Half Stack application development'
-  const courseParts = [
+
+  interface CoursePartBase {
+    name: string
+    exerciseCount: number
+  }
+
+  interface CoursePartDescription extends CoursePartBase {
+    description: string
+  }
+
+  interface CoursePartBasic extends CoursePartDescription {
+    kind: 'basic'
+  }
+
+  interface CoursePartGroup extends CoursePartBase {
+    groupProjectCount: number
+    kind: 'group'
+  }
+
+  interface CoursePartBackground extends CoursePartDescription {
+    backgroundMaterial: string
+    kind: 'background'
+  }
+
+  interface CoursePartSpecial extends CoursePartDescription {
+    requirements: string[]
+    kind: 'special'
+  }
+
+  type CoursePart =
+    | CoursePartBasic
+    | CoursePartGroup
+    | CoursePartBackground
+    | CoursePartSpecial
+
+  const courseParts: CoursePart[] = [
     {
       name: 'Fundamentals',
       exerciseCount: 10,
+      description: 'This is an awesome course part',
+      kind: 'basic',
     },
     {
       name: 'Using props to pass data',
       exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: 'group',
+    },
+    {
+      name: 'Basics of type Narrowing',
+      exerciseCount: 7,
+      description: 'How to go from unknown to string',
+      kind: 'basic',
     },
     {
       name: 'Deeper type usage',
       exerciseCount: 14,
+      description: 'Confusing description',
+      backgroundMaterial:
+        'https://type-level-typescript.com/template-literal-types',
+      kind: 'background',
+    },
+    {
+      name: 'TypeScript in frontend',
+      exerciseCount: 10,
+      description: 'a hard part',
+      kind: 'basic',
+    },
+    {
+      name: 'Backend development',
+      exerciseCount: 21,
+      description: 'Typing the backend',
+      requirements: ['nodejs', 'jest'],
+      kind: 'special',
     },
   ]
 
@@ -23,23 +87,79 @@ const App = () => {
     return <h2>{props.courseName}</h2>
   }
 
-  interface courseParts {
-    name: string
-    exerciseCount: number
-  }
-
   interface ContentProps {
-    courseParts: courseParts[]
+    courseParts: CoursePart[]
   }
 
   const Content = (props: ContentProps) => {
     return (
       <>
-        {props.courseParts.map((item, index) => (
-          <div key={index}>
-            {item.name} {item.exerciseCount}
-          </div>
-        ))}
+        <Part courseParts={props.courseParts} />
+      </>
+    )
+  }
+
+  const assertNever = (value: never): never => {
+    throw new Error(
+      `Unhandled discriminated union member: ${JSON.stringify(value)}`
+    )
+  }
+
+  const Part = (props: ContentProps) => {
+    return (
+      <>
+        {props.courseParts.map((item, index) => {
+          switch (item.kind) {
+            case 'basic':
+              return (
+                <div key={index}>
+                  <p style={{ fontWeight: 'bold' }}>
+                    {item.name} {item.exerciseCount}
+                  </p>
+                  <p style={{ fontStyle: 'italic' }}>{item.description}</p>
+                </div>
+              )
+            case 'group':
+              return (
+                <div key={index}>
+                  <p style={{ fontWeight: 'bold' }}>
+                    {item.name} {item.exerciseCount}
+                  </p>
+                  <p style={{ fontStyle: 'italic' }}>
+                    project exercises: {item.groupProjectCount}
+                  </p>
+                </div>
+              )
+            case 'background':
+              return (
+                <div key={index}>
+                  <p style={{ fontWeight: 'bold' }}>
+                    {item.name} {item.exerciseCount}
+                  </p>
+                  <p style={{ fontStyle: 'italic' }}>{item.description}</p>
+                  <p>
+                    Background Material:{' '}
+                    <a href={item.backgroundMaterial} target="_blank">
+                      {item.backgroundMaterial}
+                    </a>
+                  </p>
+                </div>
+              )
+
+            case 'special':
+              return (
+                <div key={index}>
+                  <p style={{ fontWeight: 'bold' }}>
+                    {item.name} {item.exerciseCount}
+                  </p>
+                  <p style={{ fontStyle: 'italic' }}>{item.description}</p>
+                  <p>Requirements: {item.requirements.join(', ')}</p>
+                </div>
+              )
+            default:
+              return assertNever(item)
+          }
+        })}
       </>
     )
   }
@@ -50,7 +170,12 @@ const App = () => {
       0
     )
 
-    return <div>Number of exercises: {total}</div>
+    return (
+      <div>
+        <hr />
+        Total exercises: {total}
+      </div>
+    )
   }
 
   return (
